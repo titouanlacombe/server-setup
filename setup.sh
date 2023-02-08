dcc1d10bc2ac8018b8029a785c5bab06d2c48ac1
Titouan Lacombe <titouan.lacombe99@gmail.com>
2023-02-08 20:24:17 +0100

    vfedsvdf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+# Url:
+# wget https://raw.githubusercontent.com/titouanlacombe/server-setup/master/setup.sh
+
+# Setup locale
+sudo locale-gen en_US.UTF-8
+
 sudo apt install -y git curl python3-pip python3
 
 curl https://get.docker.com/ | sudo bash
+sudo usermod -aG docker $(whoami)
 
 pip3 install docker-compose makepie
+
+git config --global user.name "Server"
+git config --global user.email "server@null.com"

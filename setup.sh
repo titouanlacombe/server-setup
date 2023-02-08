6405293a5541791fe02718172d38d0b6257eadea
Titouan Lacombe <titouan.lacombe99@gmail.com>
2023-02-08 20:48:26 +0100

    cvdscs

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ pip3 install docker-compose makepie
 
 git config --global user.name "Server"
 git config --global user.email "server@null.com"
+
+echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
+echo "export LC_ALL=C.UTF-8" >> ~/.bashrc
